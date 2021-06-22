0b245fede669554c42ef5ddce0ba81a3b84de39c
Christian <poulfrankibsen@gmail.com>
2021-06-22 20:54:39 +0200

    ascii tables ping cmd

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,6 +1,7 @@
 import { Client, Collection } from 'discord.js';
 const { Sequelize } = require('sequelize');
 import path from 'path';
+const ascii = require('ascii-table');
 import { readdirSync } from 'fs';
 import { Command, Event, Config } from '../interfaces';
 import ConfigJson from '../../config.json'
@@ -23,12 +24,20 @@ class ExtendedClient extends Client {
 
         // commands
         const commandPath = path.join(__dirname, '..', 'commands');
+        const table = new ascii().setHeading('Command', 'Status');
         readdirSync(commandPath).forEach((dir) => {
             const commands = readdirSync(`${commandPath}/${dir}`).filter((file) => file.endsWith('.ts'));
 
             for (const file of commands) {
                 const { command } = require(`${commandPath}/${dir}/${file}`);
-                this.commands.set(command.name, command);
+                const pull = this.commands.set(command.name, command);
+
+                if (pull) {
+                    table.addRow(file, '✅ Loaded!');
+                } else {
+                    table.addRow(file, '❌ -> Command failed to load, please check your work again!');
+                continue;
+                }
 
                 if(command?.aliases.length !== 0) {
                     command.aliases.forEach((alias) => {
@@ -36,6 +45,7 @@ class ExtendedClient extends Client {
                     });
                 }
             }
+            console.log(table.toString());
         });
 
         // events
@@ -43,7 +53,7 @@ class ExtendedClient extends Client {
         readdirSync(eventPath).forEach(async (file) => {
             const { event } = await import(`${eventPath}/${file}`);
             this.event.set(event.name, event);
-            console.log(event);
+            console.log('The ' + event.name + ' event is ready');
             this.on(event.name, event.run.bind(null, this));
         });
     }
--- src/commands/features/ping.ts
+++ src/commands/features/ping.ts
@@ -1,5 +1,7 @@
 import { Command } from '../../interfaces';
 import { MessageEmbed } from 'discord.js';
+const { Sequelize } = require('sequelize');
+import ConfigJson from '../../../config.json'
 
 export const command: Command = {
     name: 'ping',
@@ -7,11 +9,26 @@ export const command: Command = {
     run: async(client, message, args) => {
         const msg = await message.channel.send('🏓 Pinging...');
 
-        const embed = new MessageEmbed()
-        .setColor('#ff8956')
-        .setTitle('🏓 Pong!')
-        .setDescription(`Bot Latency is **${Math.floor(msg.createdTimestamp - message.createdTimestamp)} ms** \nAPI Latency is **${Math.round(client.ws.ping)} ms**`);
+        let dbTimeStart = new Date().getTime();
+        const db = new Sequelize(ConfigJson.postgreSQL);
+        try {
+            await db.authenticate();
+            let dbTimeEnd = new Date().getTime();
+            const dbTime = dbTimeEnd - dbTimeStart;
 
-        message.channel.send(embed);
+            const embed = new MessageEmbed()
+            .setColor('#ff8956')
+            .setTitle('🏓 Pong!')
+            .setDescription(`Bot Latency is **${Math.floor(msg.createdTimestamp - message.createdTimestamp)} ms** \nAPI Latency is **${Math.round(client.ws.ping)} ms**\nPostgreSQL Latency is **${dbTime} ms**`);
+
+            message.channel.send(embed);
+        } catch (error) {
+            const embed = new MessageEmbed()
+            .setColor('#ff8956')
+            .setTitle('🏓 Pong!')
+            .setDescription(`Bot Latency is **${Math.floor(msg.createdTimestamp - message.createdTimestamp)} ms** \nAPI Latency is **${Math.round(client.ws.ping)} ms**\nPostgreSQL connection was not established, error: ${error}`);
+
+            message.channel.send(embed);
+    }
     }
 }
