5279b5cb3f8a2f3b617be6dab298ea24313ee807
Christian <poulfrankibsen@gmail.com>
2021-06-23 14:31:09 +0200

    guildCreate

---
+++ src/events/guildCreate.ts
@@ -0,0 +1,24 @@
+import { Event } from "../interfaces";
+import database from '../database/database';
+import { initModels, guild as DbGuild, guildCreationAttributes } from '../database/models/init-models';
+import ConfigJson from '../../config.json';
+
+export const event: Event = {
+    name: 'guildCreate',
+    run: async (client, guild) => {
+        initModels(database);
+
+        const attr: guildCreationAttributes = {
+            guildID: guild.id,
+            guildName: guild.name,
+            prefix: ConfigJson.prefix,
+            tiktok: true,
+            nsfw: false,
+            leaderboard: true,
+            media: true
+        }
+
+        const newGuild = await DbGuild.create(attr)
+        console.log('New guild were added to the database. It is called: ' + newGuild.guildName + ', ID: ' + newGuild.guildID)
+    }
+}
