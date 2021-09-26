4e42a8a32bf80a7d61f9bed9abbd49dd524b857a
Christian Nielsen <68676614+banner4422@users.noreply.github.com>
2021-09-27 01:47:28 +0200

    tiktok proxy

--- src/utils/tiktok.ts
+++ src/utils/tiktok.ts
@@ -1,13 +1,18 @@
 import { MessageEmbed, MessageAttachment, Util } from "discord.js";
-import moment from "moment";
+import moment, { months } from "moment";
 import fetch from "node-fetch";
 import { getVideoMeta } from "tiktok-scraper";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const proxyArray: string[] = [`${process.env.tiktokproxy1}`, `${process.env.tiktokproxy2}`, `${process.env.tiktokproxy3}`, `${process.env.tiktokproxy4}`, `${process.env.tiktokproxy5}`]
 
 export async function tiktokEmbedding(URL: string): Promise<any> {
     try {
     const query: RegExpMatchArray = URL.match(/\bhttps?:\/\/\S+/gi);
     const finalQuery: string = query.toString();
-    const videoMeta = await getVideoMeta(finalQuery);
+    const randomProxy: string = proxyArray[Math.floor(Math.random() * months.length)]
+    const videoMeta = await getVideoMeta(finalQuery, {proxy: randomProxy})
     const video = videoMeta.collector[0];
     const videoURL = video.videoUrl;
     const headers = videoMeta.headers;
@@ -29,10 +34,12 @@ export async function tiktokEmbedding(URL: string): Promise<any> {
         .setAuthor(video.authorMeta.name, video.authorMeta.avatar, `https://www.tiktok.com/@${video.authorMeta.name}?`)
         let finalVideo = new MessageAttachment(buffer, 'video.mp4')
         return [finalVideo, embed];
-    } catch {
+    } catch (err) {
+        console.log(err)
         return;
     };
-    } catch {
+    } catch (err) {
+        console.log(err)
         return
     }
 };
